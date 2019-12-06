d0666484daacaf2382907fe4108ec7ed99c6a135
Alan Lo <lo.alan@gmail.com>
2019-12-06 13:44:25 +1100

    changing ref to influxdb by service name

--- influxdb_test.sh
+++ influxdb_test.sh
@@ -3,12 +3,17 @@
 
 set -x
 
+INFLUXDB_URL=$(minikube service --url influxdb)
+
 HOST=192.168.99.100
 PORT=31406
 DB=tutorialdb
 
 
-curl -i -XPOST "http://$HOST:$PORT/write?db=$DB" --data-binary 'asdf,key=4 value=4'
+# Make DB. Its ok to call more than once
+curl -i -XPOST "$INFLUXDB_URL/query" --data-urlencode "q=CREATE DATABASE $DB"
 
-curl -G "http://$HOST:$PORT/query?pretty=true" --data-urlencode "db=$DB" --data-urlencode 'q=select * from asdf'
+curl -i -XPOST "$INFLUXDB_URL/write?db=$DB" --data-binary 'asdf,key=4 value=4'
+
+curl -G "$INFLUXDB_URL/query?pretty=true" --data-urlencode "db=$DB" --data-urlencode 'q=select * from asdf'
 
